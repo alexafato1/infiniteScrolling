8b488ed3877759221686296aad21ec4d79df8664
Alexa fato <alexafato@MacBook-Air-Alexa.local>
2020-06-22 20:03:29 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, {useState, useRef, useCallback}from 'react';
 
 import './App.css';
+import useBookSearch from './useBookSearch';
 
 function App() {
+  const [query, serQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState('')
+  
+  const {
+    books,
+    hasMore,
+    loading,
+    error
+  } = useBookSearch(query, pageNumber)
+
+
+  const observer = useRef()
+  const lastBookElement = useCallback(node => {
+    if(loading)return
+    if(observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if(entries[0].isIntersecting && hasMore){
+        setPageNumber(prevPageNumber => prevPageNumber+1)
+        
+      }
+    })
+    if(node) observer.current.observe(node)
+  }, [loading, hasMore]) 
+
+  function handleSearch(e){
+    serQuery(e.target.value)
+    setPageNumber(1)
+  }
+
+ 
   return (
     <div >
-      
+      <input type='text' value={query} onChange={handleSearch}></input>
+      {books.map((book, index) => {
+        if(books.length === index+1){
+        return <div ref={lastBookElement} key={book}>{book}</div>
+      } else {
+      return <div  key={book}>{book}</div>
+      }
+      })}
+      <div>{loading && 'loading...'}</div>
+      <div>{error && 'error...'}</div>
+     
     </div>
   );
 }
